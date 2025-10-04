d2806fcc51254d34eeacce2a76b3c77fd09562f8
Shak-ti <shakti.coniat@gmail.com>
2025-10-04 12:37:20 +0200

    ok

---
+++ cpp06/ex00/ScalarConverter.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sconiat <sconiat@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/04 12:32:33 by sconiat           #+#    #+#             */
+/*   Updated: 2025/10/04 12:35:06 by sconiat          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+# include <iostream>
+
+class ScalarConverter {
+	public:
+			
+};
+
+#endif
