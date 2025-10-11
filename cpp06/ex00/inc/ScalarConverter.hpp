2e0804c07ad86ba872c6d9df808ad74ed76fc33c
Shak-ti <shakti.coniat@gmail.com>
2025-10-11 19:21:57 +0200

    ok

--- cpp06/ex00/inc/ScalarConverter.hpp
+++ cpp06/ex00/inc/ScalarConverter.hpp
@@ -6,13 +6,15 @@
 /*   By: sconiat <sconiat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/04 12:32:33 by sconiat           #+#    #+#             */
-/*   Updated: 2025/10/05 20:02:07 by sconiat          ###   ########.fr       */
+/*   Updated: 2025/10/11 19:09:58 by sconiat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCALARCONVERTER_HPP
 # define SCALARCONVERTER_HPP
 # include <iostream>
+# include <cstdlib>
+# include <cstring>
 
 class ScalarConverter {
 	protected:
@@ -22,7 +24,7 @@ class ScalarConverter {
 		ScalarConverter&	operator=( ScalarConverter& const );
 
 	public:
-		static void	convert( std::string const );
+		static void	convert( const std::string& );
 };
 
 #endif
--- cpp06/ex00/src/ScalarConverter.cpp
+++ cpp06/ex00/src/ScalarConverter.cpp
@@ -6,77 +6,75 @@
 /*   By: sconiat <sconiat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/04 12:32:28 by sconiat           #+#    #+#             */
-/*   Updated: 2025/10/09 17:25:56 by sconiat          ###   ########.fr       */
+/*   Updated: 2025/10/11 19:10:58 by sconiat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/ScalarConverter.hpp"
 
-ScalarConverter::ScalarConverter( void ) {
-	std::cout << "Default constructor called" << std::endl;
+ScalarConverter::ScalarConverter( void ) {}
+
+ScalarConverter::ScalarConverter( ScalarConverter& const copy ) {
+	static_cast<void>(copy); //explicitly marks copy as used in C++ style
 }
 
-ScalarConverter::ScalarConverter( ScalarConverter& const ) {
-	std::cout << "Copy constructor called" << std::endl;
+ScalarConverter::~ScalarConverter( void ) {}
+
+ScalarConverter&	ScalarConverter::operator=( ScalarConverter& const copy ) {
+	static_cast<void>(copy);
+	return (*this);
 }
 
-ScalarConverter::~ScalarConverter( void ) {
-	std::cout << "Destructor called" << std::endl;
-}
+void	printMessage( char c, int i, float f, double d ) {
 
-ScalarConverter&	ScalarConverter::operator=( ScalarConverter& const ) {
-	std::cout << "Operator = called" << std::endl;	
-}
 
-void	handleChar( std::string const input ) {
-	int		i;
-	char	c;
-	float	f;
-	double	d;	
-}
-
-void	handleInt( std::string const input ) {
-	int		i;
-	char	c;
-	float	f;
-	double	d;
-}
-
-void	handleFloat( std::string const input ) {
-	int		i;
-	char	c;
-	float	f;
-	double	d;	
-}
-
-void	handleDouble( std::string const input ) {
-	int		i;
-	char	c;
-	float	f;
-	double	d;	
-}
-
-void	handleLiterals( std::string const input ) {
 	
 }
 
-void	handleErrors( std::string const input ) {
-	std::cout << "char: impossible" << std::endl
-		<< "int : impossible" << std::endl
-		<< "float : impossible" << std::endl
-		<< "double : impossible" << std::endl;	
+void	convertChar( std::string* tab, char c ) {
+	int		i = static_cast<int>(c);
+	float	f = static_cast<float>(c);
+	double	d = static_cast<double>(c);
+
+	printMessage(c, i, f, d);
 }
 
-void	ScalarConverter::convert( std::string const input ) {
-	void (*fonctions[6]) (std::string const) = {
-			&handleChar,
-			&handleInt,
-			&handleFloat,
-			&handleDouble,
-			&handleLiterals,
-			&handleErrors
-	};
+void	convertInt( int i ) {
+	char	c = static_cast<char>(i);
+	float	f = static_cast<float>(i);
+	double	d = static_cast<double>(i);
+
+	//vérifier si char displayable, else c = 0
+	printMessage(c, i, f, d);
+}
+
+void	convertFloat( float f ) {
+	int		i = static_cast<int>(f);
+	char	c = static_cast<char>(f);
+	double	d = static_cast<double>(f);	
+
+	//vérifier si char displayable, else c = 0
+	printMessage(c, i, f, d);
+}
+
+void	convertDouble( double d ) {
+	int		i = static_cast<int>(d);
+	char	c = static_cast<char>(d);
+	float	f = static_cast<float>(d);
+
+	//vérifier si char displayable, else c = 0
+	printMessage(c, i, f, d);
+}
+
+void	ScalarConverter::convert( const std::string& input ) {
+	char	*endptr = NULL; //to malloc ?
+	double	nbr;
 	
-	int	res = 0;
-	
+	nbr = strtod(input.c_str(), &endptr);
+	if (errno == ERANGE) { 
+		std::cout << strerror(errno) << std::endl;
+		return ;
+	} else if (endptr == NULL) {
+			
+	}
 }
