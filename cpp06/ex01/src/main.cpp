0ed5d25598f744c4c62607b411811d18c0f6abf1
Shak-ti <shakti.coniat@gmail.com>
2025-10-14 14:45:44 +0200

    ok

--- cpp06/ex01/src/main.cpp
+++ cpp06/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sconiat <sconiat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 13:30:44 by sconiat           #+#    #+#             */
-/*   Updated: 2025/10/13 13:36:34 by sconiat          ###   ########.fr       */
+/*   Updated: 2025/10/14 14:43:38 by sconiat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,9 @@ int	main( void ) {
 	uintptr_t	ptr;
 
 	Test.data = 42;
-	std::cout << "Test address : " << &Test << std::endl;
 	std::cout << "Test->data : " << Test.data << std::endl;
 	ptr = Serializer::serialize(&Test);
-	std::cout << "ptr value : " << ptr << std::endl;
 	Res = Serializer::deserialize(ptr);
-	std::cout << "Res address : " << Res << std::endl;
 	std::cout << "Res->data : " << Res->data << std::endl;
 	return (0);
 }
